f2fd17f2a4faeefc28968c01c70229b810a92dc8
JakubPilchon <jakubpilchon@gmail.com>
2024-06-20 20:20:31 +0200

    First working server prototype

--- HyperServer.py
+++ HyperServer.py
@@ -1,20 +1,20 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-
-class __HyperDashRequests(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_header()
-
+from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
+import os
+from functools import partial
 
 class HyperDashServer(HTTPServer):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, host:str, port:str, directory:str, **kwargs) -> None:
         print("HyperDash 2024")
-        host = input("Enter host name: ")
-        port = int(input("Enter port number: "))
-
-        super().__init__((host, port), __HyperDashRequests, **kwargs)
+        handler = partial(SimpleHTTPRequestHandler, directory = directory)
+        
+        super().__init__((host, port), handler , **kwargs)
 
 
 if __name__ == "__main__":
-    HyperDashServer()
+    print('HyperDash 2024')
+    host = input("Provide localhost :")
+    portname = int(input('Provide port: '))
+    path = input('Provide path to Hyperdash directory: ')
+    server = HyperDashServer(host, portname, path)
+    server.serve_forever()
     
