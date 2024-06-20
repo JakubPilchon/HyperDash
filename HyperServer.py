aee1fc776865e1e644d4d199f9f270e5fb195f58
JakubPilchon <jakubpilchon@gmail.com>
2024-06-20 21:12:40 +0200

    added messages when server starts

--- HyperServer.py
+++ HyperServer.py
@@ -6,8 +6,17 @@ class HyperDashServer(HTTPServer):
     def __init__(self, host:str, port:str, directory:str, **kwargs) -> None:
         print("HyperDash 2024")
         handler = partial(SimpleHTTPRequestHandler, directory = directory)
+        self.host = host
+        self.port = port
         
-        super().__init__((host, port), handler , **kwargs)
+        super().__init__((self.host, self.port), handler , **kwargs)
+
+    def serve_forever(self, poll_interval: float = 0.5) -> None:
+        print(r"Server online at /{}:{}/ \n Ctrl+C to end".format(self.host, self.port))
+        try:
+            return super().serve_forever(poll_interval)
+        except KeyboardInterrupt:
+            print("Server Stopped due to manual shutdown")
 
 
 if __name__ == "__main__":
