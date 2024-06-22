3c773715928dd70d73be43da5b3fa5fd32b7fbd7
JakubPilchon <jakubpilchon@gmail.com>
2024-06-22 17:41:44 +0200

    impoved server architecture

--- HyperServer.py
+++ HyperServer.py
@@ -3,7 +3,7 @@ import os
 from functools import partial
 
 class HyperDashServer(HTTPServer):
-    def __init__(self, host:str, port:str, directory:str, **kwargs) -> None:
+    def __init__(self, directory:str, host:str ="localhost", port:int = 8050, **kwargs) -> None:
         print("HyperDash 2024")
         handler = partial(SimpleHTTPRequestHandler, directory = directory)
         self.host = host
@@ -12,18 +12,19 @@ class HyperDashServer(HTTPServer):
         super().__init__((self.host, self.port), handler , **kwargs)
 
     def serve_forever(self, poll_interval: float = 0.5) -> None:
-        print(r"Server online at /{}:{}/ \n Ctrl+C to end".format(self.host, self.port))
+        print("Server online at http://{}:{}/ \n Ctrl+C to end".format(self.host, self.port))
         try:
             return super().serve_forever(poll_interval)
         except KeyboardInterrupt:
+            self.server_close()
             print("Server Stopped due to manual shutdown")
 
 
 if __name__ == "__main__":
     print('HyperDash 2024')
-    host = input("Provide localhost :")
-    portname = int(input('Provide port: '))
+    host = input("Provide host adress:")
+    #portname = int(input('Provide port: '))
     path = input('Provide path to Hyperdash directory: ')
-    server = HyperDashServer(host, portname, path)
+    server = HyperDashServer(path, host)
     server.serve_forever()
     
